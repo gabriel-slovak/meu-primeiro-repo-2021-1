494257b189e8dcd9eff9dd111a8ea4d6493cb299
Gabriel Slovak <slovakgabriellopes@gmail.com>
2021-03-03 15:40:45 -0300

    finalização

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace meu_primeiro_repo_2021_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
